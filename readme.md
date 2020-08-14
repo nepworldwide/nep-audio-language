de07663cb0b798673de37b7f9de5d20530cee2e2
sugarfixx <ingar.torsrud@me.com>
2020-08-14 13:55:04 +0200

    Init commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Audio Language
+
+
+##Install
+If you are starting a blank project from a clean composer.json
+```angular2html
+
+{
+   "require":{
+      "nepworldwide/nep-audio-language":"0.1"
+   },
+   "repositories":[
+      {
+         "type":"vcs",
+         "url":"git@github.com:nepworldwide/nep-audio-language.git"
+      }
+   ]
+}
+```
+
+Run
+```angular2html
+composer require nepworldwide/nep-audio-language
+```
---
+++ src/NepAudioLanguage/AudioLanguage.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 14/08/2020
+ * Time: 13:24
+ */
+
+namespace NepAudioLanguage;
+
+use NepAudioLanguage\AudioLanguageDictionaries;
+
+class AudioLanguage
+{
+    public $name;
+
+    public $iso639_1;
+
+    public $iso639_3;
+
+
+    public function __construct($code = null)
+    {
+        if ($code) {
+            $this->createObject($code);
+        }
+    }
+
+    public function createObject($code)
+    {
+        $dictionary = new AudioLanguageDictionaries();
+        $alpha2 = $dictionary->resolve($code);
+        $this->setIso639_1($alpha2);
+        $this->setIso639_3($dictionary->key_iso_639_1[$alpha2]);
+        $this->setName($dictionary->nameFromKey[$alpha2]);
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setIso639_1($iso_1)
+    {
+        $this->iso639_1 = $iso_1;
+    }
+
+    public function getIso639_1()
+    {
+        return $this->iso639_1;
+    }
+
+    public function setIso639_3($iso_3)
+    {
+        $this->iso639_3 = $iso_3;
+    }
+
+    public function getIso639_3()
+    {
+        return $this->iso639_3;
+    }
+
+}
