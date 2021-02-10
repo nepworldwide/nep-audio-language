591236120d5984efb33d859704ec05f2e485f58c
sugarfixx <ingar.torsrud@me.com>
2021-02-10 17:35:31 +0100

    Add tests

---
+++ tests/LanguageTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class LanguageTest
+{
+
+    use NepAudioLanguage\AudioLanguage;
+    use NepAudioLanguage\AudioLanguageDictionaries;
+
+class LanguageTest extends TestCase
+{
+    public function testShouldReturnLanguageList()
+    {
+        $this->get("language", []);
+        $this->seeStatusCode(200);
+    }
+
+    public function testShouldReturnLanguage()
+    {
+        $this->get("language/eng", []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'name',
+            'iso_639_1',
+            'iso_639_3'
+        ]);
+    }
+
+    public function testShouldReturnDefaultLanguage()
+    {
+        $this->get("language/qpr", []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'name',
+            'iso_639_1',
+            'iso_639_3'
+        ]);
+        $this->seeJsonEquals([
+            "name" => "Undetermined",
+            "iso_639_1" => "ud",
+            "iso_639_3" => "und"
+        ]);
+    }
+
+    public function testConsistencyInAudioLanguageDictionary()
+    {
+        $dict = new AudioLanguageDictionaries();
+        $languageList = $dict->listIsoDashThree();
+        foreach ($languageList as $listedLanguage  => $code )  {
+            $language = new AudioLanguage($code);
+            $this->assertEquals($code, $language->iso_639_3);
+        }
+    }
+}
